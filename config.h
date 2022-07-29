4fc6be59f3bb243a212cf48564bd50bf39c7bcd0
FZZkill <changzhijie11@outlook.com>
2022-07-29 09:21:08 +0800

    choose font

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 3;        /* 0 means no bar */
 static const int topbar             = 3;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" };
-static const char dmenufont[]       = "JetBrains Mono:size=14";
+static const char *fonts[]          = { "JetBrainsMonoExtraBold:size=14" };
+static const char dmenufont[]       = "JetBrainsMonoExtraBold:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -25,6 +25,9 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+/* Lockfile */
+static char lockfile[] = "/tmp/dwm.lock";
+
 /* Lockfile */
 static char lockfile[] = "/tmp/dwm.lock";
 
@@ -86,6 +89,7 @@ static Key keys[] = {
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
     { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
     { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+    { MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
     { MODKEY,                       XK_space,  setlayout,      {0} },
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
