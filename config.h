741a1161c857ba02d8c051d7d3bd87e5c43f6202
FZZkill <changzhijie11@outlook.com>
2022-08-28 21:18:39 +0800

    Update

--- config.h
+++ config.h
@@ -12,9 +12,9 @@ static const char selfgcolor[]      = "#eeeeee";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
 
-static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int gappx = 5;    /* gaps between windows */
-static const unsigned int snap = 32;    /* snap pixel */
+static const unsigned int snap = 30;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const int vertpad = 5;           /* vertical padding of bar */
